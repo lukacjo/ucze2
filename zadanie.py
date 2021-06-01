a3c1d9fd358a8128e5aaa82e33e0fd8f5bee693a
Lukasz Iwanski <lukasziwanski@outlook.com>
2021-06-01 23:39:21 +0200

    liczenie poteg

--- zadanie.py
+++ zadanie.py
@@ -3,3 +3,18 @@ slownik={
     "piekarnia": ["chleb","paczek","bulki"],
     "warzywniak": ["marchew", "seler","rukola"]
 }
+itemy=slownik.items()
+
+print("Lista zakupów")
+
+count=0
+
+for key, value in itemy:
+    print("Ide do", key.capitalize(),"kupuje tu następujące rzeczy:",value )
+    count += len(value)
+print("W sumie kupuję",count,"produktów.")
+
+#zadanie 2
+licz=[]
+poteg=[]
+i=0
